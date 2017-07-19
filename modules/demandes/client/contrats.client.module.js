7e890cb278d2a7628ffce1a968ec3ed686c2f5b4
Hakim SOCK <serigne.sock@neticoa.fr>
2017-07-19 16:45:45 +0000

    feat(antus): Init project

--- modules/demandes/client/contrats.client.module.js
+++
@@ -1,9 +0,0 @@
-(function (app) {
-  'use strict';
-  app.registerModule('contrats', ['core']);
-  app.registerModule('contrats.services', ['ngResource']);
-  app.registerModule('contrats.model');
-  app.registerModule('contrats.routes');
-  app.registerModule('contrats.directives');
-  app.registerModule('contrats.filters');
-}(ApplicationConfiguration));
